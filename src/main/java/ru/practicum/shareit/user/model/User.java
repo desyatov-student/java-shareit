ffad062dd6c011b953e282a96ba62f46d5ca61d3
Alexander Desyatov <desyatovav@yandex.ru>
2024-09-17 23:28:04 +0300

    Update User.java

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -30,15 +30,6 @@ public class User {
     @Column(name = "name", nullable = false)
     private String name;
 
-//    @Column(name = "first_name", nullable = false)
-//    private String firstName;
-//
-//    @Column(name = "last_name")
-//    private String lastName;
-
     @Column(name = "registration_date")
     private Instant registrationDate = Instant.now();
-
-//    @Enumerated(EnumType.STRING)
-//    private UserState state;
 }
